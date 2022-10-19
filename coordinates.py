599dd9cd02a559562fc320c0743ab4e87590eccc
Maarten <maarten7post@gmail.com>
2022-10-19 13:23:03 +0200

    blacked

--- coordinates.py
+++ coordinates.py
@@ -8,15 +8,15 @@ def convert_value(value):
 
     if value.endswith("N") or value.endswith("E"):
 
-        return int(value[:-1]) / 100.
+        return int(value[:-1]) / 100.0
 
     if value.endswith("S") or value.endswith("W"):
 
-        return -int(value[:-1]) / 100.
+        return -int(value[:-1]) / 100.0
 
 
 def get_ports_coordinates():
-    #dfg = pd.read_csv("Downloads/code-list_csv.csv")
+    # dfg = pd.read_csv("Downloads/code-list_csv.csv")
     dfg = pd.read_csv("data/code-list_csv.csv")
     dfg = dfg.dropna(subset=["Coordinates"])
 
@@ -33,27 +33,30 @@ def get_ports_coordinates():
 
     return dfg
 
+
 def plot_ports(list_of_ports):
     world = geopandas.read_file(geopandas.datasets.get_path("naturalearth_lowres"))
     dfg = get_ports()
-    dfg = dfg[dfg['UNLocode'].isin(list_of_ports)]
-    ax = dfg.plot(color='k', marker='o', zorder=2)
+    dfg = dfg[dfg["UNLocode"].isin(list_of_ports)]
+    ax = dfg.plot(color="k", marker="o", zorder=2)
     world.plot(ax=ax, zorder=1)
     plt.show()
 
+
 def ports_to_x_dx(list_of_ports):
     dfg = get_ports_coordinates()
-    dfg = dfg[dfg['UNLocode'].isin(list_of_ports)]
-   
+    dfg = dfg[dfg["UNLocode"].isin(list_of_ports)]
+
     list_ = []
     for i, port_origin in enumerate(list_of_ports[:-1]):
         port_origin_xy = dfg.query("UNLocode == @port_origin").iloc[0].geometry
-        x, y = port_origin_xy.x, port_origin_xy.y 
-        
+        x, y = port_origin_xy.x, port_origin_xy.y
+
         port_dest = list_of_ports[i + 1]
         port_dest_xy = dfg.query("UNLocode == @port_dest").iloc[0].geometry
         dx, dy = port_dest_xy.x - x, port_dest_xy.y - y
         list_.append((x, y, dx, dy))
     return list_
+
 
 a = ports_to_x_dx(["NLRTM", "CNXMG", "CNNBG"])
