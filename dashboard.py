49b1b2ddf966320d03ab0bcf39856f54e6b44ad9
Maarten <maarten7post@gmail.com>
2022-10-19 16:09:02 +0200

    updates to prototype

--- dashboard.py
+++ dashboard.py
@@ -1,17 +1,20 @@
 import streamlit as st
 from PIL import Image
-from coordinates import plot_ports
+from route import plot_ports
+from plaatje import plaatje
+from get_ports_list import get_ports_list
 import pandas as pd
 
-st.title("Container route")
+st.title("Container paspoort")
 
 xls = pd.ExcelFile("data/Dataset-data-interoperability_rev-HA.xlsx")
 containers = pd.read_excel(xls, xls.sheet_names[0])["Containernummer"]
 container = st.selectbox("Container", containers)
-st.write(container)
 
-fig, ax = plot_ports(["NLRTM", "CNXMG", "CNNBG"])
+shipping_ports, customs_ports = get_ports_list(container)
+fig, ax = plot_ports(shipping_ports, customs_ports, unloc=True)
 st.pyplot(fig)
 
-image = Image.open("ship.jpg")
-st.image(image, caption="container ship MEARSK DAANIS", use_column_width="auto")
+ship_image_file = plaatje(container)
+image = Image.open(ship_image_file)
+st.image(image, caption="container ship", use_column_width="auto")
